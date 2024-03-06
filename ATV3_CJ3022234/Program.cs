c396afa29f3bb1192df874a733ea95619d249e5a
CJ3022234 <CJ3022234@LAB01-PC38>
2024-03-06 08:54:02 -0300

    Adicionar arquivos de projeto.

---
+++ ATV3_CJ3022234/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite a nota:");
+        double nota =
+        Convert.ToDouble(Console.ReadLine());
+
+        //Verificação se a nota é maior do que 6
+
+        if (nota >= 6)
+        {
+            Console.WriteLine("O aluno foi aprovado!");
+        }
+        else
+        {
+            Console.WriteLine("O aluno foi reprovado.");
+        }
+
+    }
+}
+
+        
+
